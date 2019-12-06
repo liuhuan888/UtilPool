3cbeb0a5b36c639b7988fec7cc3452c931f3a407
liuhuan888 <837959438@qq.com>
2019-12-06 18:47:41 +0800

    流工具类增加写入文件方法

--- liuhuan-common-util/src/main/java/com/liuhuan/common/utils/StreamUtil.java
+++ liuhuan-common-util/src/main/java/com/liuhuan/common/utils/StreamUtil.java
@@ -2,10 +2,12 @@ package com.liuhuan.common.utils;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -13,10 +15,10 @@ import java.util.List;
 
 public class StreamUtil {
 
-	//关闭流
-	public static void closeAll(AutoCloseable ...autoCloseables){
+	// 关闭流
+	public static void closeAll(AutoCloseable... autoCloseables) {
 		for (AutoCloseable autoCloseable : autoCloseables) {
-			if(autoCloseable!=null){
+			if (autoCloseable != null) {
 				try {
 					autoCloseable.close();
 				} catch (Exception e) {
@@ -26,58 +28,78 @@ public class StreamUtil {
 			}
 		}
 	}
-	
-	//读取文本文件	
-	public static String readTextFile(File file){
+
+	// 读取文本文件
+	public static String readTextFile(File file) {
 		String str = "";
 		InputStream inputStream = null;
 		try {
 			inputStream = new FileInputStream(file);
 			byte[] b = new byte[1024];
-			while(inputStream.read(b)>0){
-				str+=new String(b);
+			while (inputStream.read(b) > 0) {
+				str += new String(b);
 			}
 			return str;
 		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
-		}finally {
+		} finally {
 			closeAll(inputStream);
 		}
 	}
-	
-	//重载读取文本文件	
-	public static String readTextFile(String fileFullName){
+
+	// 重载读取文本文件
+	public static String readTextFile(String fileFullName) {
 		File file = new File(fileFullName);
-		return readTextFile(file); 
+		return readTextFile(file);
 	}
-	
-	//按行读取文本文件
-	public static List<String> readTextFileList(File file){
+
+	// 按行读取文本文件
+	public static List<String> readTextFileList(File file) {
 		List<String> list = new ArrayList<String>();
 		BufferedReader br = null;
 		try {
 			br = new BufferedReader(new FileReader(file));
-			while (br.read()!=-1) {
+			while (br.read() != -1) {
 				list.add(br.readLine());
 			}
 			return list;
 		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
-		}finally {
+		} finally {
 			closeAll(br);
 		}
-		
+
 	}
-	
-	//重载按行读取文本文件
-	public static List<String> readTextFileList(String fileFullName){
+
+	// 重载按行读取文本文件
+	public static List<String> readTextFileList(String fileFullName) {
 		File file = new File(fileFullName);
 		return readTextFileList(file);
 	}
+
+	// 写入文本文件
+	public static void writerFile(String content, File file, boolean append) {
+		BufferedWriter bufferedWriter = null;
+		try {
+			bufferedWriter = new BufferedWriter(new FileWriter(file,append));
+			bufferedWriter.write(content);
+			bufferedWriter.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			closeAll(bufferedWriter);
+		}
+	}
 	
+	// 重载写入文本文件
+	public static void writerFile(String content, String fileFullName, boolean append) {
+		File file = new File(fileFullName);
+		writerFile(content, file, append);
+	}
+
 	public static void main(String[] args) {
-		System.out.println(readTextFileList("E:/MyGit/hello.txt.txt"));
+		writerFile("123456789zxcvvbnbn",new File("C:\\Users\\83795\\Desktop\\aa.txt"),true);
 	}
 }
